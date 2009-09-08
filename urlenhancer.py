6d5d13423a286d6ffa12b1504ba0a8a2eebfba48
commanda <amanda.wixted@gmail.com>
2009-09-07 17:17:47 -0700

    templatized the html. added new logo.png.

--- urlenhancer.py
+++ urlenhancer.py
@@ -1,34 +1,9 @@
-import cgi, random, sys, urllib
+import cgi, random, sys, urllib, os
 from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
 
-selectJs = """
-<script language="JavaScript" type="text/javascript">
-<!--
-	function select_text()
-	{
-		el = document.getElementById('url');
-		if (el.createTextRange) 
-		{
-			var oRange = el.createTextRange();
-	   		oRange.moveStart("character", 0);
-			oRange.moveEnd("character", el.value.length);
-			oRange.select();
-		}
-		else if (el.setSelectionRange) 
-		{
-			el.setSelectionRange(0, el.value.length);
-		}
-		el.focus();
-	}
 
-	function PageInit() {
-		select_text();
-	}
-        window.onload = PageInit;
-//-->
-</script>
-"""
 
 dickRules = (
   "~*@=*8",
@@ -46,15 +21,10 @@ def get_server(request):
     address += ":%s" % port
   return address + "/"
 
-def header(request):
-  html = "<html><head><title>100% All-natural URL enhancement!</title></head><body>" + selectJs + """
-    <div style="width:400px;margin:0 auto;align:center;">
-      <a href="/"><img src="http://wxbanker.org/dicks/logo.png" align="center" border="0"/></a><br/>"""
-  return html
-
-def footer():
-  return """</div></body></html>"""
-
+def templatize(response):
+  template_values = {"response": response}
+  return template.render('template.html', template_values)
+    
 class UrlMap(db.Model):
   enhancedPhrase = db.StringProperty()
   destinationUrl = db.StringProperty()
@@ -64,20 +34,20 @@ class UrlMap(db.Model):
 class MainPage(webapp.RequestHandler):
   def get(self):
     response = """
-      <form action="/enhance" method="post">
-        <div><textarea name="content" rows="3" cols="60"></textarea></div>
-        <div><input type="submit" value="Enhance that embarrassing URL!"></div>
+      <form action="/emojied" method="post">
+        <div><input type="text" size="30" name="content"/></div>
+        <div><input type="submit" value="Emojify!"></div>
       </form>"""
-    self.response.out.write(header(self.request) + response + footer())
+    self.response.out.write(templatize(response))
 
 class Reporter(webapp.RequestHandler):
   def get(self):
     urls = db.GqlQuery("SELECT * FROM UrlMap")
     response = "<ul>"
     for url in urls:
-      response += "<li>%s => %s (%i)</li>" % (url.enhancedPhrase, url.destinationUrl, url.hits)
+      response += "<li>%s => \'%s\' (%i)</li>" % (url.enhancedPhrase, url.destinationUrl, url.hits)
     response += "</ul>"
-    self.response.out.write(header(self.request) + response + footer())
+    self.response.out.write(templatize(response))
 
 class Enhancer(webapp.RequestHandler):
   def generatePhrase(self):
@@ -85,8 +55,7 @@ class Enhancer(webapp.RequestHandler):
     phrase = ""
     for i in range(0,4):
       phrase += characters[random.randrange(0, len(characters))]
-    
-#    phrase = "8------D-" + str(random.randint(1,sys.maxint))
+
     return phrase
 
   def generateUrl(self, phrase):
@@ -101,12 +70,9 @@ class Enhancer(webapp.RequestHandler):
     mapObj.put()
 
     newUrl = self.generateUrl(phrase)
-
-    
-    response = 'Wow, we\'re not really sure how you had friends with such an insignificant URL!</br>'
-    response += '<br/>We highly recommend not showing that to anyone, instead use:<br/><br/>'
-    response += '</div><center><input type="text" value="'+newUrl+'" size="60" id="url"/><div><br/>Sorry this URL is so lame, an actual enhancing algorithm is coming soon.'
-    self.response.out.write(header(self.request) + response + footer())
+    response = 'Here\'s your emojied URL:<br/>' 
+    response += '</div><center><input type="text" value="'+newUrl+'" size="30" id="url"/><div>'
+    self.response.out.write(templatize(response))
 
 class Char(webapp.RequestHandler):
   def get(self):
@@ -115,8 +81,7 @@ class Char(webapp.RequestHandler):
     for n in range(0, len(characters), 5):
       output += characters[n:n+5] + "<br />"
     output += "</div>"
-    
-    self.response.out.write(header(self.request) + output + footer())
+    self.response.out.write(templatize(response))
 
 class Redirecter(webapp.RequestHandler):
   def get(self):
@@ -124,7 +89,7 @@ class Redirecter(webapp.RequestHandler):
     try:
       url = db.GqlQuery("SELECT * FROM UrlMap WHERE enhancedPhrase = :1 LIMIT 1", key)[0]
     except IndexError:
-      self.response.out.write(header(self.request) + "Sorry friends, no dice on that one (%s)."%key + footer())
+      self.response.out.write(templatize(response))
     else:
       url.hits += 1
       url.put()
@@ -135,8 +100,8 @@ class Redirecter(webapp.RequestHandler):
 
 application = webapp.WSGIApplication(
   [('/', MainPage),
-   ('/enhance', Enhancer),
-   ('/tellmeallaboutdicks', Reporter),
+   ('/emojied', Enhancer),
+   ('/tellmeallaboutemojied', Reporter),
    ('/char', Char),
    ('/.*', Redirecter)
    ],
